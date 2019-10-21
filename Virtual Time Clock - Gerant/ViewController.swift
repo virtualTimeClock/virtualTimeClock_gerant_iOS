dfa0685adf32f42e0694d59d931d686d22afc4ae
Guillaume NIRLO <guillaume.nirlo@hotmail.fr>
2019-10-21 14:47:10 +0400

    Initial Commit

---
+++ Virtual Time Clock - Gerant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Time Clock - Gerant
+//
+//  Created by Guillaume Nirlo on 10/21/19.
+//  Copyright © 2019 Guillaume Nirlo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
