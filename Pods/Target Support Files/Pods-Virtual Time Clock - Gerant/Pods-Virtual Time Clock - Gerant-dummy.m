8377fa1db4cad60833b35e3382d9ae1511e3afa9
Guillaume NIRLO <guillaume.nirlo@hotmail.fr>
2019-10-28 17:22:28 +0400

    Ajout Interface graphique connection

---
+++ Pods/Target Support Files/Pods-Virtual Time Clock - Gerant/Pods-Virtual Time Clock - Gerant-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Virtual_Time_Clock___Gerant : NSObject
+@end
+@implementation PodsDummy_Pods_Virtual_Time_Clock___Gerant
+@end
--- Virtual Time Clock - Gerant/ViewController.swift
+++ Virtual Time Clock - Gerant/ViewController.swift
@@ -11,13 +11,31 @@ import Firebase
 
 class ViewController: UIViewController {
     
-    @IBOutlet 
+    // MARK: Outlet
 
+    
+    @IBOutlet weak var mdpTF: UITextField!
+    
+    @IBOutlet weak var mailTF: UITextField!
+    @IBOutlet weak var conectTF: UIButton!
+    
+    // MARK: Properties
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
-
+    
+    // MARK: Private functions
+    
+    private func setupButton(){
+        conectTF.layer.cornerRadius = 20
+    }
+    
+    // MARK: Action
+    
+    
 
 }
 
